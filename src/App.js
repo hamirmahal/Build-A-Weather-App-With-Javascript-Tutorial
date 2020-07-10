d1f72986ea7c9d19adc10d9079d2ff05b677f26a
Hamir Mahal <hamirmahal@gmail.com>
2020-07-09 19:23:48 -0700

    Updated .svg, iOS home screen icon, title, favicon

--- src/App.js
+++ src/App.js
@@ -3,20 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let latitude, longitude;
-  navigator.geolocation.getCurrentPosition( position  =>
-    { latitude = position.coords.latitude;
-      longitude = position.coords.longitude;
-      alert(latitude);    alert(longitude); },    ()  =>
-    { latitude = 37.63;
-      longitude = -122.46;
-      alert(latitude);    alert(longitude); }         );
+  const key = 'fd9d9c6418c23d94745b836767721ad1/';
+  const ds = 'https://api.darksky.net/forecast/';
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  let lat, long;
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      lat = position.coords.latitude;
+      long = position.coords.longitude;
+      alert(lat);
+      alert(long);
+      const api = `${proxy}${ds}${key}/${lat},${long}`;
+      fetch(api).then(  response  =>  response.json() )
+        .then( rInJSON => alert( rInJSON.timezone ) );
+    },
+    ()  =>  {
+      lat = 37.63;
+      long = -122.46;
+      alert(lat);
+      alert(long);
+      const api = `${proxy}${ds}${key}/${lat},${long}`;
+      fetch(api).then(  response  =>  response.json() )
+        .then( rInJSON => alert( rInJSON.timezone ) );
+    }
+  );
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          View the tutorial Hamir is using to create this
+          web application <a href=
+          'https://www.youtube.com/watch?v=wPElVpR1rwA'
+          rel='noopener noreferrer'
+          target='_blank'>here</a>.
         </p>
         <a
           className="App-link"
