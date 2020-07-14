bbc29dcee0ed28c183ee2431a8cada315c819249
Hamir Mahal <hamirmahal@gmail.com>
2020-07-13 22:15:37 -0700

    Added theme for foggy, cloudy, partly-cloudy days!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import graySun from './foggy-day-sun.png';
 import moon from './Full-Moon-PNG-Transparent-Img.png';
 import sun from './the-sun-symbol.png';
 import './App.css';
 
 function App() {
-  const [itIsDaytime,setItIsDaytime] = useState(false);
+  const [ weather , setWeather ] = useState();
+  console.log('Latest build: Monday, July 13, 2020');
   const key = 'fd9d9c6418c23d94745b836767721ad1/';
   const ds = 'https://api.darksky.net/forecast/';
   const proxy = 'https://cors-anywhere.herokuapp.com/';
@@ -35,20 +37,42 @@ function App() {
       console.log(j.daily);
       console.log(j.daily.data);
       console.log(j.daily.data[0]);
-      const {sunriseTime,sunsetTime} = j.daily.data[0];
-      const {temperature, time, summary} = j.currently;
-      if ( sunriseTime <= time && time <= sunsetTime )
-        setItIsDaytime(true);                   } );  }
+      const {icon, temperature, summary} = j.currently;
+      setWeather(icon);                   } );  }
+
+  function getImageFor(typeOfWeather)
+  { if ( typeOfWeather === 'partly-cloudy-day' ||
+      typeOfWeather==='cloudy'||typeOfWeather==='fog' )
+      return <img src={graySun} className="App-logo"
+        alt="gray sun" />;
+        
+    if (typeOfWeather === 'clear-day')
+      return <img src={sun} className="App-logo"
+        alt="sun" />;
+    
+    return <img src={moon} className="App-logo"
+      alt="moon" />; }
+  
+  function getStylingFor(typeOfWeather)
+  { let backgroundColor = 'black';
+    let color = 'white';
+
+    if ( typeOfWeather === 'partly-cloudy-day' ||
+      typeOfWeather==='cloudy'||typeOfWeather==='fog' )
+      backgroundColor = 'gray';
+        
+    if (typeOfWeather === 'clear-day')
+    { backgroundColor = 'aqua';
+      color = 'black'; }
+    
+    return {  backgroundColor , color }; }
 
   return (
     <div className="App">
-      <header className="App-header" style={{
-      backgroundColor: itIsDaytime ? 'aqua':'black',
-      color:itIsDaytime&&'black',position:'relative'}}>
-        { itIsDaytime ? <img src={sun}
-          className="App-logo" alt="sun" /> : <img
-          src={moon} className="App-logo" alt="moon"/>}
-        <h1 style={temperatureStyle}>74</h1>
+      <header className="App-header"
+        style={getStylingFor(weather)}>
+        {getImageFor(weather)}
+        {/* <h1 style={temperatureStyle}>74</h1> */}
         {/* <h1 style={degreeStyle}>°</h1> */}
         <p>
           View the tutorial Hamir is using to create
@@ -70,16 +94,15 @@ function App() {
   );
 }
 
-const temperatureStyle = {
-  // backgroundColor: 'pink',
-  color: 'black',
-  fontSize: '20vmin',
-  height: '30vmin',
-  left: '50%',
-  position: 'absolute',
-  top: '30%',
-  transform: 'translate(-50%, -50%)'
-}
+// const temperatureStyle = {
+//   alignItems: 'center',
+//   backgroundColor: 'pink',
+//   color: 'black',
+//   display: 'flex',
+//   fontSize: '10vmin',
+//   height: '40vmin',
+//   position: 'absolute',
+// }
 
 // const degreeStyle = {
 //   backgroundColor: 'aqua',
