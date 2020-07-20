c878ee8075781f456c2ea74b5ffda87f0ef87a72
Hamir Mahal <hamirmahal@gmail.com>
2020-07-19 23:45:45 -0700

    Added acknowledgements for images used!

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import umbrella from './OpenUmbrella_PNG_Clip_Art.png';
 import './App.css';
 
 export default function App() {
+  const [ gTemp , setGlobalTemperature ] = useState(0);
+  const [apiIsLoading, setAPIIsLoading]=useState(true);
   const [ summary , setSummary ] = useState();
-  const [ gTemp , setGlobalTemperature ] = useState(135);
-  const [ timezone , setTimezone ] = useState();
+  const [ tZone , setTimezone ] = useState();
   const [ units , setUnits ] = useState('F');
-  const [ uTitle, setuTitle ] = useState('Fahrenheit');
+  const [ uType,setUnitType ] = useState('Fahrenheit');
   const [ weather , setWeather ] = useState('');
   const key = 'fd9d9c6418c23d94745b836767721ad1/';
   const ds = 'https://api.darksky.net/forecast/';
@@ -39,10 +40,11 @@ export default function App() {
       const { timezone } = j;           console.log(j);
       const {icon, summary, temperature} = j.currently;
       console.log(j.currently);
-      setSummary(summary);
       setGlobalTemperature(temperature);
+      setSummary(summary);
       setTimezone(timezone);
-      setWeather(icon);                }        );    }
+      setWeather(icon);
+      setAPIIsLoading(false);          }        );    }
 
   function getImageFor(typeOfWeather)
   { let alt = "sun"; let c = "App-logo"; let src = sun;
@@ -79,46 +81,40 @@ export default function App() {
   function toggleUnits()
   { if (units === 'F')
     { setGlobalTemperature( (gTemp - 32) / 1.8 );
-      setUnits('C');      setuTitle('Celsius'); }
+      setUnits('C');    setUnitType('Celsius'); }
       
     if (units === 'C')
     { setGlobalTemperature( gTemp + 273.15 );
-      setUnits('K');   setuTitle('Kelvin'); }
+      setUnits('K'); setUnitType('Kelvin'); }
 
     if (units === 'K')
     { setGlobalTemperature((gTemp-273.15) * 1.8 + 32);
-      setUnits('F');        setuTitle('Fahrenheit'); }}
+      setUnits('F');      setUnitType('Fahrenheit'); }}
 
-  return (
-    <div className="App">
-      <header className="App-header"
-        style={getStylingFor(weather)}>
-        {getImageFor(weather)}
-        <h1 className="Temperature">{Math.round(gTemp)}
-          °<abbr className="Unit" onClick={toggleUnits}
-          title={uTitle}>{units} </abbr> </h1>
-        <p>
-          {summary}
-          <br />
-          <br />
-          Timezone: {timezone}
-          <br />
-          <br />
-          <a  href  =
-          'https://www.youtube.com/watch?v=wPElVpR1rwA'
-          rel='noopener noreferrer' target='_blank'>
-          View the tutorial</a> Hamir is using to
-          create this web application.
-        </p>
-        <a 
-          className="App-link"
-          href="https://darksky.net/poweredby/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by Dark Sky
-        </a>
-      </header>
-    </div>
-  );
-}
+  return ( <div className = "App">
+    <header className = "App-header" style =
+    {getStylingFor(weather)}> {getImageFor(weather)}
+    {!apiIsLoading && <h1 className = "Temperature"
+    onClick = {toggleUnits}>  {Math.round(gTemp)}°<abbr
+    className="Unit" title={uType}>{units}</abbr></h1>}
+    <p>{summary} <br/><br/>Timezone: {tZone} <br/><br/>
+    <a href =
+    'https://www.youtube.com/watch?v=wPElVpR1rwA'
+    rel='noopener noreferrer' target='_blank'> View the
+    tutorial</a> used to create this application.
+    <br /> <br />I would like to thank <a href =
+    'https://www.clipartqueen.com/sun-clipart.html'
+    rel='noopener noreferrer' target='_blank'>
+    ClipArtQueen</a> for the super sun clipart; thank
+    you very much! My gratitude also goes toward <a
+    href = 'https://gallery.yopriceville.com/'
+    rel='noopener noreferrer' target='_blank'>Gallery 
+    Yopriceville</a> for the useful umbrella image.
+    And, a heartfelt thank-you goes out to <a href = 
+    'https://www.pngarts.com/explore/31675'
+    rel='noopener noreferrer' target='_blank'>PNG ARTS
+    </a> for the marvelous moon .png!</p>
+    <a className = "App-link"
+    href = "https://darksky.net/poweredby/"
+    rel="noopener noreferrer" target="_blank">
+    Powered by Dark Sky</a> </header> </div> );       }
